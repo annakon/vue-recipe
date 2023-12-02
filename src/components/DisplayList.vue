0feddd1e017570d6a213a583e8efcca910b33e65
denis <denis.baklykov@boxberry.ru>
2023-12-02 17:18:43 +0300

    хорошо отображается сетка

--- src/components/DisplayList.vue
+++ src/components/DisplayList.vue
@@ -1,5 +1,7 @@
 <template>
-  <RecipeItem v-for="item in props.recipeList" :key="item.id" :recipe="item"> </RecipeItem>
+  <div class="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:gap-x-8">
+    <RecipeItem v-for="item in props.recipeList" :key="item.id" :recipe="item"> </RecipeItem>
+  </div>
 </template>
 
 <script setup>
--- src/components/RecipeItem.vue
+++ src/components/RecipeItem.vue
@@ -1,9 +1,9 @@
 <template>
         <div class="border shadow-lg mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-8 lg:max-w-md lg:px-8">
-          <div class="overflow-hidden">
+          <div class="aspect-h-1 aspect-w-1 overflow-hidden rounded-md bg-gray-200 lg:aspect-none lg:h-72 lg:w-72">
             <img :src="props.recipe.recipe.image" alt="Front of men&#039;s Basic Tee in black." class="hover:scale-125 h-full w-full object-cover object-center lg:h-full lg:w-full">
           </div>
-          <div class="p-5">
+          <div class="p-5 sm:w-72">
              <h2 class="text-2xl truncate font-extrabold">
                   {{ props.recipe.recipe.label }}
              </h2>
