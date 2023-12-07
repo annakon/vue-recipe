d5ce49fefcfd753a90cf7c59b25a6d2a05c36ab8
denis <denis.baklykov@boxberry.ru>
2023-12-07 18:25:19 +0300

    внешний вид формы

--- src/App.vue
+++ src/App.vue
@@ -4,19 +4,19 @@ import {RouterLink, RouterView} from 'vue-router'
 
 <template>
   <header>
-    <div class="bg-[url('src/assets/conscious-design-IMMHJRp4dcM-unsplash.jpg')] bg-left bg-cover grid">
-      <nav>
+    <div class="bg-[url('src/assets/conscious-design-IMMHJRp4dcM-unsplash.jpg')] bg-center bg-cover grid">
+      <nav class="text-right">
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/favorites">Favorites</RouterLink>
       </nav>
       <form class="md:w-1/2 2xl:w-1/3 bg-orange-400 my-32 p-10 rounded-lg space-y-6">
         <div class="space-y-4">
-          <label>Name or ingredients</label>
-          <input/>
+          <label class="block text-white uppercase font-extrabold text-lg">Name or ingredients</label>
+          <input placeholder="Name or ingredient: eg. chicken, egg, etc.." type="text" class="p-3 w-full rounded-lg focus:outline-none"/>
         </div>
         <div class="space-y-4">
-          <label>Category</label>
-          <select>
+          <label class="block text-white uppercase font-extrabold text-lg">Category</label>
+          <select class="p-3 w-full rounded-lg focus:outline-none">
             <option>--Select--</option>
           </select>
         </div>
