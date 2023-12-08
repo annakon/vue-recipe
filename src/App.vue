539f52de99755acc170f1fae7dd10c05a5d3c357
denis <denis.baklykov@boxberry.ru>
2023-12-08 18:48:20 +0300

    отступы

--- src/App.vue
+++ src/App.vue
@@ -6,11 +6,11 @@ import {RouterLink, RouterView} from 'vue-router'
   <header>
     <div class="bg-[url('src/assets/conscious-design-IMMHJRp4dcM-unsplash.jpg')] bg-center bg-cover grid">
       <div class="container mx-auto py-5 px-16">
-        <nav class="justify-end flex gap-4 text-white mr-24 mt-16 mb-2">
+        <nav class="justify-end flex gap-4 text-white">
           <RouterLink class="uppercase font-bold" to="/">Home</RouterLink>
           <RouterLink class="uppercase font-bold" to="/favorites">Favorites</RouterLink>
         </nav>
-        <form class="md:w-1/2 2xl:w-1/3 bg-orange-400 md:ml-24 m-4  p-10 rounded-lg space-y-6">
+        <form class="md:w-1/2 2xl:w-1/3 bg-orange-400 p-10 rounded-lg space-y-6">
           <div class="space-y-4">
             <label class="block text-white uppercase font-extrabold text-lg">Name or ingredients</label>
             <input placeholder="Name or ingredient: eg. chicken, egg, etc.." type="text"
