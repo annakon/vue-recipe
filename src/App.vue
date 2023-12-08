9593aaf3d6ae1ff813b254470a82e1d271958b36
denis <denis.baklykov@boxberry.ru>
2023-12-08 18:33:13 +0300

    шрифты и отступы

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,11 @@ import {RouterLink, RouterView} from 'vue-router'
 <template>
   <header>
     <div class="bg-[url('src/assets/conscious-design-IMMHJRp4dcM-unsplash.jpg')] bg-center bg-cover grid">
-      <nav class="text-right">
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/favorites">Favorites</RouterLink>
+      <nav class="justify-end flex gap-4 text-white mr-24 mt-16 mb-2">
+        <RouterLink class="uppercase font-bold" to="/">Home</RouterLink>
+        <RouterLink class="uppercase font-bold" to="/favorites">Favorites</RouterLink>
       </nav>
-      <form class="md:w-1/2 2xl:w-1/3 bg-orange-400 m-16 p-10 rounded-lg space-y-6">
+      <form class="md:w-1/2 2xl:w-1/3 bg-orange-400 md:ml-24 m-4  p-10 rounded-lg space-y-6">
         <div class="space-y-4">
           <label class="block text-white uppercase font-extrabold text-lg">Name or ingredients</label>
           <input placeholder="Name or ingredient: eg. chicken, egg, etc.." type="text" class="p-3 w-full rounded-lg focus:outline-none"/>
