42c446b31316ee1db82974e55320edf0c91dbefd
denis <denis.baklykov@boxberry.ru>
2024-02-21 16:48:45 +0300

    анимация размер

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -55,4 +55,17 @@ const close= ()=> {
  .modal-animation-leave-to {
    opacity: 0;
  }
+ .modal-animation-inner-enter-active {
+   transition: all .3s cubic-bezier(.52,0.02,.19,1.02) 0.15s;
+ }
+ .modal-animation-inner-leave-active {
+   transition: all .3s cubic-bezier(.52,0.02,.19,1.02);
+ }
+ .modal-animation-inner-enter-from {
+   opacity: 0;
+   transform: scale(0.8);
+ }
+ .modal-animation-inner-leave-to {
+   transform: scale(0.8);
+ }
 </style>
